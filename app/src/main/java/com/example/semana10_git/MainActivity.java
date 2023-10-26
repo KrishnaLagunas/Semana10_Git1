f1f32b598f04474e9384006f1a90a83f36cb2115
Laboratorio <krishna58lagunas@gmail.com>
2023-10-26 16:51:57 -0300

    mas hello word

--- app/src/main/java/com/example/semana10_git/MainActivity.java
+++ app/src/main/java/com/example/semana10_git/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
+        System.out.println("Hello word!");
     }
 }
